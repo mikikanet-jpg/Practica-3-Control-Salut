633d8291d8496a67f0b39ef627f52dd2c55f994f
Miguel Sanchez Cuadros <Mikikanet@gmail.com>
2025-10-07 17:53:29 +0200

    Correcio de posicio d'unes variables i finalitzacio de comprovacio de valors

--- PE03_Control_Salut.java
+++ PE03_Control_Salut.java
@@ -7,8 +7,6 @@ public class PE03_Control_Salut {
        String option;
        String name;
        int age;
-       Float weight;
-       Float height;
     
        System.out.println("\n Benvingut al programa sobre el control de la salut general fisica");
        System.out.println("Sisplau escull l'opcio que vols utilitzar: ");
@@ -28,16 +26,36 @@ public class PE03_Control_Salut {
                 if (name.trim().equals("")) {
                     System.out.println("Error: El nom no pot quedar buit!");
                 } else {
-                System.out.println("Sisplau introdueix la teva edat: ");
-                age = j.nextInt();
-                if (age >= 120)
-                {
-                    System.out.println("Error:");
-                }
-                System.out.println("Sisplau introdueix el teu pes: ");
-                weight = j.nextFloat();
-                System.out.println("Sisplau introdueix la teva alçada: ");
-                height = j.nextFloat();             
+                    System.out.println("Sisplau introdueix la teva edat: ");
+                    age = j.nextInt();
+                    if (age >= 120)
+                    {
+                        System.out.println("Error: L'edat ha de ser un enter positiu <=120");
+                    } else {
+                        System.out.println("Sisplau introdueix el teu pes: ");
+                        // llegim com a text per poder substituir la coma per punt
+                        String pesText = j.next();
+
+                        // canviem la coma per un punt (així Java el pot convertir a float)
+                        pesText = pesText.replace(",", ".");
+
+                        // convertim a float
+                        float weight = Float.parseFloat(pesText);
+                        if (weight <= 0 || weight > 400) {
+                            System.out.println("Error: El pes ha de ser un decimal positiu raonable.");
+                        } else {
+                                System.out.println("Sisplau introdueix la teva alçada: ");   
+                                try {
+                                float height = j.nextFloat();
+
+                                if (height < 0.5 || height > 2.5) {
+                                    System.out.println("Error: L'alçada ha de ser un decimal positiu entre 0.5 i 2.5 metres.");
+                                } else {}
+                                } catch (InputMismatchException e) {
+                                    System.out.println("Error: Format numèric invàlid.");
+                                }        
+                        }
+                    }
                 }
             case "b": //Modificar dades
             case "c": //Visualitzar dades
