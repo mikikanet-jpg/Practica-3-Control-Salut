db0d5ed39f685ee455ca5c0ad6aed208da2bfc84
Miguel Sanchez Cuadros <Mikikanet@gmail.com>
2025-10-09 19:44:11 +0200

    Començant el case b, definint variables noves per poder fer el case b i posant el case de sortida

--- PE03_Control_Salut.java
+++ PE03_Control_Salut.java
@@ -7,63 +7,100 @@ public class PE03_Control_Salut {
        String option;
        String name;
        int age;
-    
-       System.out.println("\n Benvingut al programa sobre el control de la salut general fisica");
-       System.out.println("Sisplau escull l'opcio que vols utilitzar: ");
-       System.out.println("a) Introduir les dades");
-       System.out.println("b) Modificar les dades");
-       System.out.println("c) Visualitzar les dades");
-       System.out.println("");
-        option = j.next();
-        
-        switch (option) {
-            case "a": // Introduir dades
+       boolean sortida = false;
+       float weight;
+       String pesText = "";
+ 
+        while (sortida = false)
+            System.out.println("\n Benvingut al programa sobre el control de la salut general fisica");
+            System.out.println("Sisplau escull l'opcio que vols utilitzar: ");
+            System.out.println("a) Introduir les dades");
+            System.out.println("b) Modificar les dades");
+            System.out.println("c) Visualitzar les dades");
+            System.out.println("");                  
+            System.out.println("");
+            option = j.next();
+            
+            switch (option) {
+             case "a": // Introduir dades
+             if (sortida = false)
                 j.nextLine(); // Neteja el buffer del next()
 
-                System.out.println("\nSisplau introdueix el teu nom:");
-                name = j.nextLine();
+                    System.out.println("\nSisplau introdueix el teu nom:");
+                    name = j.nextLine();
 
-                if (name.trim().equals("")) {
-                    System.out.println("Error: El nom no pot quedar buit!");
-                } else {
-                    System.out.println("Sisplau introdueix la teva edat: ");
-                    age = j.nextInt();
-                    if (age >= 120)
-                    {
-                        System.out.println("Error: L'edat ha de ser un enter positiu <=120");
+                    if (name.trim().equals("")) {
+                        System.out.println("Error: El nom no pot quedar buit!");
                     } else {
-                        System.out.println("Sisplau introdueix el teu pes: ");
-                        // llegim com a text per poder substituir la coma per punt
-                        String pesText = j.next();
-
-                        // canviem la coma per un punt (així Java el pot convertir a float)
-                        pesText = pesText.replace(",", ".");
-
-                        // convertim a float
-                        float weight = Float.parseFloat(pesText);
-                        if (weight <= 0 || weight > 400) {
-                            System.out.println("Error: El pes ha de ser un decimal positiu raonable.");
+                        System.out.println("Sisplau introdueix la teva edat: ");
+                        age = j.nextInt();
+                        if (age >= 120)
+                        {
+                            System.out.println("Error: L'edat ha de ser un enter positiu <=120");
                         } else {
-                                System.out.println("Sisplau introdueix la teva alçada: ");   
-                                try {
-                                float height = j.nextFloat();
+                            System.out.println("Sisplau introdueix el teu pes: ");
+                            pesText = j.next().replace(",", ".");
+                            weight = Float.parseFloat(pesText);
+                            j.nextLine(); // neteja buffer
+                            if (weight <= 0 || weight > 400) {
+                                System.out.println("Error: El pes ha de ser un decimal positiu raonable.");
+                            } else {
+                                    System.out.println("Sisplau introdueix la teva alçada (m): ");   
+                                    try {
+                                    float height = j.nextFloat();
 
-                                if (height < 0.5 || height > 2.5) {
-                                    System.out.println("Error: L'alçada ha de ser un decimal positiu entre 0.5 i 2.5 metres.");
-                                } else {}
-                                } catch (InputMismatchException e) {
-                                    System.out.println("Error: Format numèric invàlid.");
-                                }        
+                                    if (height < 0.5 || height > 2.5) {
+                                        System.out.println("Error: L'alçada ha de ser un decimal positiu entre 0.5 i 2.5 metres.");
+                                    } else {}
+                                    } catch (InputMismatchException e) {
+                                        System.out.println("Error: Format numèric invàlid.");
+                                    }        
+                            }
                         }
                     }
-                }
-            case "b": //Modificar dades
-            case "c": //Visualitzar dades
-            default:
-                break;
-            
-       } 
+                case "b": //Modificar dades
+                if (sortida = false)
+                    
+                    j.nextLine(); //neteja de buffer
+
+                    System.out.println("Escull quina dada vols modificar:");
+                    System.out.println("1. Nom");
+                    System.out.println("2. Edat");
+                    System.out.println("3. Pes (kg)");
+                    System.out.println("4. Alçada (m)");
+                    int opcioMod = j.nextInt();
+                    j.nextLine();
+
+                    switch (opcioMod) {
+                        case 1:
+                            System.out.println("Nou nom: ");
+                            name = j.nextLine();
+                       break;
+                        case 2: 
+                            System.out.println("Nova edat");
+                            age = j.nextInt();
+                        break;
+                        case 3:
+                            System.out.println("Nou pes (kg):");
+                            pesText = j.next().replace(",", ".");
+                            weight = Float.parseFloat(pesText);
+                            j.nextLine();
+                        break;
+                        case 4:
+                            System.out.println("Nova alçada (m)");
+                    }
+                    
+
+                case "c": //Visualitzar dades
+
+                case "x": //sortida del bucle
+                sortida = true;
+                System.out.println("Sortint del programa...");
+                default:
+                    break;
+                
+          }  
+        }
     }
-}
-    
+        
 
